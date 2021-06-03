49e0fe149ee0aa53f495ee3564e9464d36f849a3
Shehab Solyman <Shehabamgad134@gmail.com>
2021-06-03 13:25:44 +0200

    Portion 1

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,139 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Member;
 import java.util.Hashtable;
+import java.util.Scanner;
+import java.util.Vector;
 
 public class Main {
-    private Hashtable<String, String> Memory;
+    public Hashtable<String, String> Memory;
 
-    public static String[] readFile(){
+    public String readFile(String fileName){
+        //read the file and return it as a string
+        return "";
+    }
+
+    public void writeFile(String fileName, String data){
 
     }
 
-    public static void writeFile(){
-
+    public void writeOutput(String print){
+        System.out.println(print);
     }
 
-    public static void writeOutput(){
-
+    public String readInput(){
+        Scanner sc = new Scanner(System.in);
+        String userInput = sc.nextLine();
+        return userInput;
     }
 
-    public static String readInput(){
-
+    public String readMemory(String x){
+        return Memory.get(x);
     }
 
-    public static String readMemory(){
-
+    public void writeMemory(String assigned, String assignee){
+        Memory.put(assigned, assignee);
     }
 
-    public static void writeMemory(){
-
+    public void assign(Vector<String> data){
+        String x = data.get(1);
+        String v = readMemory(data.get(1));
+        if(data.size()==3 && data.get(1).equals("readFile")){
+            x = readFile(data.get(2));
+        }
+        else if(data.get(1).equals("input")){
+            x = readInput();
+        }
+        else if(v!=null){
+            x = v;
+        }
+        writeMemory(data.get(0), x);
     }
 
-    public static void assign(){
-
+    public void add(String assigned, String assignee){
+        //check if they are memory variables first
+        // parse after retrieving
     }
 
-    public static void add(){
-        
+    public void parser(String program) throws IOException {
+
+        File file = new File("src/"+program);
+
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String st;
+        while ((st = br.readLine()) != null){
+            String[] line = st.split(" ");
+
+            int skipper = 0 ;
+            String inst = "";
+            Vector<String> instruction = new Vector<>();
+            for (int j = 0 ; j < line.length ; j++){
+
+                switch (line[j]) {
+                    case "assign":
+                        skipper = skipper + 2;
+                        if (inst=="")
+                            inst = "assign";
+                        break;
+                    case "print":
+                        skipper++;
+                        if (inst=="")
+                            inst = "print";
+                        break;
+                    case "add":
+                        skipper = skipper + 2;
+                        if (inst=="")
+                            inst = "add";
+                        break;
+                    case "readFile":
+                        skipper++;
+                        if (inst=="")
+                            inst = "readFile";
+                        break;
+                    case "writeFile":
+                        skipper = skipper + 2;
+                        if (inst=="")
+                            inst = "writeFile";
+                        break;
+                    default:
+                        instruction.add(line[j]);
+                        skipper--;
+                        break;
+                } // end of switch for reading line ;
+
+                if (skipper == 0){
+                    switch (inst){
+                        case "assign":
+                            assign(instruction);
+                            break;
+                        case  "print":
+                            writeOutput(instruction.get(0));
+                            break;
+                        case "add":
+                            add(instruction.get(0),instruction.get(1));
+                            break;
+                        case "readFile":
+                            readFile(instruction.get(0));
+                            break;
+                        case "writeFile":
+                            writeFile(instruction.get(0),instruction.get(1));
+                            break;
+                    }//end of switch to choose which instruction
+
+                }// end of if to excute line
+
+            }// end of looping in line
+
+        }// end of loop of file
+
+    }// end of parse method
+
+    public static void main(String[] args) {
+        Hashtable<String, String> x= new Hashtable<>();
+
+        System.out.println(x.get("r"));
     }
 
 }
