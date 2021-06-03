d64b062a1a90ec27d8f96cc1507f7b662b762f91
Shehab Solyman <Shehabamgad134@gmail.com>
2021-06-03 12:03:05 +0200

    initializing methods

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Hashtable;
+
+public class Main {
+    private Hashtable<String, String> Memory;
+
+    public static String[] readFile(){
+
+    }
+
+    public static void writeFile(){
+
+    }
+
+    public static void writeOutput(){
+
+    }
+
+    public static String readInput(){
+
+    }
+
+    public static String readMemory(){
+
+    }
+
+    public static void writeMemory(){
+
+    }
+
+    public static void assign(){
+
+    }
+
+    public static void add(){
+        
+    }
+
+}
