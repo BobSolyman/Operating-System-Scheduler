80d4754f8a755a7b504ee83b548db6e759656c7b
Mahmoud-Hesham99 <mhm1h9@gmail.com>
2021-06-20 20:46:41 +0200

    milestone 1 fix

--- src/OS.java
+++ src/OS.java
@@ -1,9 +1,6 @@
 import java.io.*;
 import java.lang.reflect.Member;
-import java.util.Hashtable;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.Vector;
+import java.util.*;
 
 public class OS {
     public Hashtable<String, String> Memory;
@@ -76,27 +73,14 @@ public class OS {
         Memory.put(assigned, assignee);
     }
 
-    public void assign(Vector<String> data){
+    public void assign(String a, String b){
 
-        String x = data.get(1);
-        String v = readMemory(data.get(1));
-        if(data.size()==3 && data.get(1).equals("readFile")){
-            try {
-                x = readFile(data.get(2));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        else if(data.get(1).equals("input")){
-            x = readInput();
-        }
-        else if(v!=null){
+        String x = b;
+        String v = readMemory(b);
+        if(v!=null){
             x = v;
         }
-        writeMemory(data.get(0), x);
-//        for(Map.Entry m: Memory.entrySet()){
-//            System.out.println(m);
-//        }
+        writeMemory(a, x);
     }
 
     public void add(String assigned, String assignee){
@@ -124,68 +108,50 @@ public class OS {
         String st;
         while ((st = br.readLine()) != null){
             String[] line = st.split(" ");
-            int skipper = 0 ;
-            String inst = "";
-            Vector<String> instruction = new Vector<>();
+            Stack <String> s = new Stack<>();
             for (int j = 0 ; j < line.length ; j++){
-                switch (line[j]) {
-                    case "assign":
-                        skipper = skipper + 2;
-                        if (inst=="")
-                            inst = "assign";
-                        break;
-                    case "print":
-                        skipper++;
-                        if (inst=="")
-                            inst = "print";
-                        break;
-                    case "add":
-                        skipper = skipper + 2;
-                        if (inst=="")
-                            inst = "add";
-                        break;
-                    case "readFile":
-                        skipper++;
-                        if (inst.equals("assign")) {
-                            instruction.add(line[j]);
-                            skipper--;
-                        }
-                        if (inst=="")
-                            inst = "readFile";
-                        break;
-                    case "writeFile":
-                        skipper = skipper + 2;
-                        if (inst=="")
-                            inst = "writeFile";
-                        break;
-                    default:
-                        instruction.add(line[j]);
-                        skipper--;
-                        break;
-                } // end of switch for reading line ;
+                s.push(line[j]);
+            }
+            while(!s.isEmpty()){
+                String b = null ;
+                String a = null ;
+                String inst = null ;
+                b = s.pop();
+                if (b.equals("input")){
+                    b = readInput() ;
+                    s.push(b);
+                    continue;
+                }
+                if (s.peek().equals("readFile")){
+                    inst = s.pop();
+                    s.push(readFile(b));
+                    continue;
+                }
+                if (s.peek().equals("print")){
+                    inst = s.pop();
+                    writeOutput(b);
+                    continue;
+                }
+                a = s.pop();
+                if (s.peek().equals("assign")){
+                    inst = s.pop();
+                    assign(a, b);
+                    continue;
+                }
+                if (s.peek().equals("add")){
+                    inst = s.pop();
+                    add(a, b);
+                    continue;
+                }
+                if (s.peek().equals("writeFile")){
+                    inst = s.pop();
+                    writeFile(a, b);
+                    continue;
+                }
 
-                if (skipper == 0){
-                    switch (inst){
-                        case "assign":
-                            assign(instruction);
-                            break;
-                        case "print":
-                            writeOutput(instruction.get(0));
-                            break;
-                        case "add":
-                            add(instruction.get(0),instruction.get(1));
-                            break;
-                        case "readFile":
-                            readFile(instruction.get(0));
-                            break;
-                        case "writeFile":
-                            writeFile(instruction.get(0),instruction.get(1));
-                            break;
-                    }//end of switch to choose which instruction
 
-                }// end of if to execute line
+            }// end of stack
 
-            }// end of looping in line
 
         }// end of loop of program
 
